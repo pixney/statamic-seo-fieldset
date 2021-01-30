71e1ee51cdada1ed1067db22c7e0a5e3ed3df18b
William Åström <william@pixney.com>
2021-01-30 14:36:57 +0100

    .

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DynamicToken;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Dynamic Token route for posting a form with Ajax.
+Route::get('/!/DynamicToken/refresh', [DynamicToken::class, 'getRefresh']);
+
+// The Sitemap route to the sitemap.xml
+Route::statamic('/sitemap.xml', 'sitemap/sitemap', [
+    'layout'       => null,
+    'content_type' => 'application/xml'
+]);
+
+// The Search route to display search results with `views/search.antlers.html`.
+// Route::statamic('/search', 'search', [
+//     'title' => 'Search results'
+// ]);
